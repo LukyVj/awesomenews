f95fc117f27057f413436fffa977fb6c5cbda379
WebGate <lucas.bonomi@gmail.com>
2013-07-14 16:17:14 +0200

    rm config

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* conf.php File : Connection to the database
----------------------------------------*/
-
-	$dbh = mysql_connect ("mysql51-32.pro", "webgatemain", "xXhJU5OL") or die ('Impossible de se connecter à la base de donnès : '); 
-	mysql_select_db ("webgatemain");
-	$mail="contact@web-gate.fr";
-?>
